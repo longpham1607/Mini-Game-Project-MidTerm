5226c7b9c8a1709ccba4647eac1bf99ec00153ec
Đỗ Thanh Tùng <56503231+nomian2001@users.noreply.github.com>
2022-06-27 23:27:53 +0700

    add screen menu

---
+++ Assets/screens/menu/MainMenuController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGame(){
+        Application.LoadLevel("Level1");
+    }
+}
